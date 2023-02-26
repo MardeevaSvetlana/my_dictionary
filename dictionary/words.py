92f599b484ef4d7fd5ecf4b562c9541bab36699c
MardeevaSvetlana <94744898+MardeevaSvetlana@users.noreply.github.com>
2023-02-26 14:34:39 +0700

    Update words.py

--- dictionary/words.py
+++ dictionary/words.py
@@ -3,8 +3,8 @@ words={'here':'здесь','there':'там','these':'эти','those':'те', 'ag
        'cost':'стоить', 'absolutely':'абсолютно','feel':'чувствовать себя',
        'near':'рядом','pound':'фунт','together':'вместе','hear':'слышать', 'call':'звонить','watch':'смотреть что- то',
        'happen':'случиться','voice':'голос', 'always':'всегда','question':'вопрос','umbrella':'зонт','eat':'есть',
-       'different':'разное','juice':'сок','daughter':'дочь','swimming':'плавание', ' correctly':'корректно',
+       'different':'разный','juice':'сок','daughter':'дочь','swimming':'плавание', ' correctly':'корректный',
        'become':'стать','businessman':'бизнесмен','abroad':'заграница', 'tomorrow':'завтра','education':'образование',
-       'future':'будущее','verb':'глагол', 'strange':'странно','sell':'продавать','worry':'беспокоиться','teach':'преподавать',
-       'buy':'покупать','why':'почему','when':'когда','where':'где','another':'другое', 'other':'другие','try':'пытаться',
+       'future':'будущее','verb':'глагол', 'strange':'странный','sell':'продавать','worry':'беспокоиться','teach':'преподавать',
+       'buy':'покупать','why':'почему','when':'когда','where':'где','another':'другой', 'other':'другие','try':'пытаться',
        'useful':'полезный','late':'поздно','early':'рано'}
